b0b725ed4e82dd96b8f06b3b2b59b63249158938
Pedro Elias Maldini <pmaldini@udesa.edu.ar>
2024-11-04 14:25:14 -0300

    microcambio

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,17 +3,14 @@ const op = db.Sequelize.Op;
 
 let homeController = {
   index: (req, res) => {
-
     db.Car.findAll()
       .then((results) => {
         res.render("home", { results, title: "Cars" });
       })
       .catch((err) => console.log(err));
-
   },
 
   detalle: (req, res) => {
-
     let id = req.params.id;
 
     db.Car.findByPk(id)
@@ -21,18 +18,16 @@ let homeController = {
         res.render("detalle", { results, title: results.name });
       })
       .catch((err) => console.log(err));
-
   },
   filtrado: (req, res) => {
-  let marca = req.params.marca;
-  let filtro = {where: [{brand:marca}]}
+    let marca = req.params.marca;
+    let filtro = { where: [{ brand: marca }] };
 
     db.Car.findAll(filtro)
       .then((results) => {
         res.render("home", { results, title: results.brand });
       })
       .catch((err) => console.log(err));
-
   },
 };
 
