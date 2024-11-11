a8ec88bddaa8c20678b4790117564b9068c09747
NicoEtche2004 <netchepare@udesa.edu.ar>
2024-11-11 17:18:14 -0300

    orden de productos en el index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,18 +3,24 @@ const db = require("../database/models");
 
 const indexController = {
   index: function (req, res) {
-    db.Producto.findAll({ include: [{ association: "usuario" }] })
+    let filtro = {
+      order: [['createdAt', 'DESC']],
+      include: [{ association: "usuario" }]
+    };
+    db.Producto.findAll(filtro)
       .then((result) => {
         return res.render("index", { result: result });
         return res.send(result); // para ver si funciona
       })
-
       .catch((err) => {});
   },
 
   detalle: function (req, res) {
     let id = req.params.id;
-    let filtro = { where: [{ id: id }] };
+    let filtro = {
+      where: [{ id: id }],
+    };
+    
 
     db.Producto.findOne(filtro)
       .then((result) => {
